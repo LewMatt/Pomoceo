a9abeb0e02df66c65e4d0549fc894a0ea3470735
LewMatt <lewmateush@gmail.com>
2022-05-23 16:39:42 +0200

    Add project files.

---
+++ Pomoceo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pomoceo.Startup))]
+namespace Pomoceo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
