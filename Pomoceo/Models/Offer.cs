b25f29e65cfaedb3c739f5898c33548ec8937571
w59050 <lewmatt@gmail.com>
2022-05-24 15:16:47 +0200

    Files updated

---
+++ Pomoceo/Models/Offer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pomoceo.Models
+{
+    public class Offer
+    {
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+
+    }
+}
